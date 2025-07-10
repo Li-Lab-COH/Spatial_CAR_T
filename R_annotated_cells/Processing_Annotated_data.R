a83610988b2dc74fb4cc08c2392fd1d7f57d3052
Jonathan Anzules <jonanzule@gmail.com>
2025-07-09 22:34:01 -0700

    .

--- R_annotated_cells/Processing_Annotated_data.R
+++ R_annotated_cells/Processing_Annotated_data.R
@@ -8,8 +8,15 @@ library(SeuratDisk)
 seurat_obj <- readRDS("~/1Work/RoseLab/Spatial/CAR_T/data/annotated.all.cells_PricemanLab.RDS")
 
 colnames(seurat_obj@meta.data)
+table(seurat_obj$sctype_classification)
 
-unique(seurat_obj@meta.data$sctype_classification)
+
+barplot(
+  sort(table(seurat_obj@meta.data$sctype_classification), decreasing = TRUE),
+  las = 2,             # rotation
+  cex.names = 0.7,     # text size
+  col = "steelblue"
+)
 
 # or equivalently
 unique(Idents(seurat_obj))
